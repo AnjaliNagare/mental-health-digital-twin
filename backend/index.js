a43629557d746b257167f27510c8621bad25dd79
AnjaliNagare <anjalinagare20@gmail.com>
2026-04-15 13:04:17 +0200

    cors fixed

--- backend/index.js
+++ backend/index.js
@@ -2,9 +2,7 @@ const express = require("express");
 const cors = require("cors");
 
 const app = express();
-app.use(cors({
-  origin: "https://mental-health-digital-twin-mkoj.vercel.app/"
-}));
+app.use(cors());
 app.use(express.json());
 
 
